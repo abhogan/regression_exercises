80d0573fc440feb0f0e966cf1a4527aca0c41260
abhogan <hogan.alexandra@gmail.com>
2019-02-15 17:11:36 +0000

    new examples

--- chapter5_logistic_regression.R
+++ chapter5_logistic_regression.R
@@ -1,3 +1,15 @@
+library(R2OpenBUGS)
+library(arm)
+
+######################################
+
+# Functions
+
+jitter.binary <- function(a, jitt=.05){
+  ifelse (a==0, runif (length(a), 0, jitt), runif (length(a), 1-jitt, 1))
+}
+
+######################################
 
 # wells in Bangladesh
 dat <- read.delim("wells.dat", header = TRUE, sep = " ")
@@ -42,6 +54,26 @@ dat$cent_arsenic <- dat$arsenic - mean_arsenic
 
 fit.5 <- glm(dat$switch ~ dat$cent_dist100 + dat$cent_arsenic + dat$cent_dist100:dat$cent_arsenic, family=binomial(link="logit"))
 display(fit.5)
+
 # probability of switching for average distance and arsenic level
 invlogit(fit.5$coefficients[1])
 
+# add social predictor - education
+dat$educ4 <- dat$educ/4
+dat$cent_educ4 <- dat$educ4 - mean(dat$educ4)
+
+fit.6 <- glm(dat$switch ~ dat$cent_dist100 + dat$cent_arsenic + dat$cent_educ4 + dat$cent_dist100:dat$cent_arsenic + dat$cent_arsenic:dat$cent_educ4 + dat$cent_dist100:dat$cent_educ4, family=binomial(link="logit"))
+display(fit.6)
+
+# Residuals
+# plots of raw residuals from logistic regression generally not useful. Instead, we plot binned residuals. 
+
+dat$log_arsenic <- log(dat$arsenic)
+dat$cent_log_arsenic <- dat$log_arsenic - mean(dat$log_arsenic)
+
+fit.7 <- glm(dat$switch ~ dat$cent_dist100 + dat$cent_log_arsenic + dat$cent_educ4 + dat$cent_dist100:dat$cent_log_arsenic + dat$cent_log_arsenic:dat$cent_educ4 + dat$cent_dist100:dat$cent_educ4, family=binomial(link="logit"))
+display(fit.7)
+
+# average predictive comparisons on the probability scale
+fit.10 <- glm (dat$switch ~ dat$dist100 + dat$arsenic + dat$educ4, family = binomial(link = "logit"))
+display(fit.10)
